55fdfe674492393d212a7875bd036485a3c338b6
MooseV2 <anthony@desouza.li>
2019-01-18 21:41:25 -0500

    Views and components

---
+++ frontend/src/Views/Dashboards/GenericDashboard/GenericDashboard.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import FixedMenu from './FixedMenu'
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <FixedMenu/>
+            </div>
+        );
+    }
+}
+
+export default Login;
