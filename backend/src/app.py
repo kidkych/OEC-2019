99bc9f30b776097184c28b9a899ebc26fe21a2f2
Chirag Karia <trichiragkaria@gmail.com>
2019-01-19 01:50:27 -0500

    do stuff

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,12 +7,15 @@ from sqlobject import *
 
 app = FlaskAPI(__name__)
 
+# Define db location
 DB_FILE = os.path.abspath('data.db')
 DB_FILE = "sqlite:" + DB_FILE
 
+# connect to sqlite
 conn = connectionForURI(DB_FILE)
 sqlhub.processConnection = conn
 
+# define Users table
 class User(SQLObject):
     userName = StringCol()
     firstName = StringCol()
@@ -20,9 +23,11 @@ class User(SQLObject):
     email = StringCol()
     password = StringCol()
 
+# create Users table if fresh database
 if not User.tableExists():
     User.createTable()
 
+# define Patients table
 class Patient(SQLObject):
     user = ForeignKey('User')
     ward = IntCol()
@@ -45,9 +50,11 @@ class Patient(SQLObject):
             'discharge': self.discharge if self.discharge is not None else "not discharged"
         }
 
+# create Patients table if fresh database
 if not Patient.tableExists():
     Patient.createTable()
 
+# define Nurse table
 class Nurse(SQLObject):
     user = ForeignKey('User')
     ward = IntCol()
@@ -62,15 +69,27 @@ class Nurse(SQLObject):
             'ward': self.ward
         }
 
-
+# create Nurse table if fresh database
 if not Nurse.tableExists():
     Nurse.createTable()
 
 # @app.route("/users/add", methods=['POST'])
 def add_user(username, first, last, email, passw):
+    ''' Helper function to add a user to db when a new nurse or patient is added
+
+    :param username: username for resource access
+    :param first: first name of user
+    :param last: last name of user
+    :param email: email of user
+    :param passw: chosen password
+    :return: User object created by SQLObject
+    '''
+
+    # ensure no fields are missing
     if ('' or None) in [username, first, last, email, passw]:
         abort(Response("Ensure all fields are filled."))
 
+    # Ensure user doesn't exist with same username
     if User.select().count() != 0:
         if len(list(User.select(User.q.userName==username))) > 0:
             abort(Response("Username exists"))
@@ -85,12 +104,17 @@ def add_user(username, first, last, email, passw):
 
 @app.route("/nurse/add/", methods=['POST'])
 def add_nurse():
+    '''Flask endpoint for adding nurses
+
+    :return: Success message if successful
+    '''
     _userName = request.data.get('userName')
     _firstName = request.data.get('firstName')
     _lastName = request.data.get('lastName')
     _email = request.data.get('email')
     _password = request.data.get('password')
 
+    # create new user object
     user = add_user(
         _userName,
         _firstName,
@@ -99,6 +123,7 @@ def add_nurse():
         _password
     )
 
+    # create nurse object with associated user object
     Nurse(
         ward=int(request.data.get('ward')),
         user=user
@@ -108,12 +133,17 @@ def add_nurse():
 
 @app.route("/patient/add/", methods=['POST'])
 def add_patient():
+    '''Flask endpoint to add a patient
+
+    :return: Success message if successful
+    '''
     _userName = request.data.get('userName')
     _firstName = request.data.get('firstName')
     _lastName = request.data.get('lastName')
     _email = request.data.get('email')
     _password = request.data.get('password')
 
+    # create new user object
     user = add_user(
         _userName,
         _firstName,
@@ -124,6 +154,7 @@ def add_patient():
 
     curr_time = datetime.datetime.now()
 
+    # create patient object with associated user
     Patient(
         ward=int(request.data.get('ward')),
         admission=curr_time,
@@ -135,22 +166,51 @@ def add_patient():
 
 @app.route("/patient/get/", methods=['GET'])
 def get_patients():
-    _params = request.args
+    '''Flask endpoint to list all patients
 
-    ward = int(_params['ward'])
-    result = list(Patient.select(Patient.q.ward==ward))
-
+    :return: list of patient dictionaries
+    '''
+    result = list(Patient.select())
     return [x.to_dict() for x in result]
 
 @app.route("/nurse/get/", methods=['GET'])
 def get_nurses():
-    _params = request.args
+    '''Flask endpoint to list all nurses
 
-    ward = int(_params['ward'])
-    result = list(Nurse.select(Nurse.q.ward==ward))
-
+    :return: list of nurse dictionaries
+    '''
+    result = list(Nurse.select())
     return [x.to_dict() for x in result]
 
-# @app.route("/patient/update/", methods=['POST'])
-# def update_patient():
-    
+@app.route("/patient/update/", methods=['POST'])
+def update_patient():
+    ''' Update selected patient (selected patient provided through userName in POST
+
+    :return: "Updated" if successful
+    '''
+    user = request.data.get('userName')
+
+    # get patient that matches provided username
+    quer = list(Patient.select(
+        AND(User.q.userName==user, Patient.q.userID==User.q.id)
+    ))[0]
+
+    # current time
+    time = datetime.datetime.now()
+
+    # updated notes
+    notes = request.data.get('notes')
+    discharge = request.data.get('discharge')
+
+    # update notes if provided
+    if notes is not None:
+        quer.notes = notes
+
+    # update last checked time
+    quer.lastChecked = time
+
+    # if discharge is true, store discharge time
+    if bool(discharge) is True:
+        quer.discharge = time
+
+    return "Updated"
